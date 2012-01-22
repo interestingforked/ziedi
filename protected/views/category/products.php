812ba7de99d4cd97b8d5f67c86f0505e633e2ee7
Pavels <pavel@csscat.com>
2012-01-23 01:33:34 +0200

    fixed views

--- protected/views/category/products.php
+++ protected/views/category/products.php
@@ -38,48 +38,5 @@ for ($i = 0; $i < $rows; $i++) {
     echo '</tr>';
     echo '<tr><td colspan="3" style="height:5px;padding:0;"></td></tr>';
 }
-
-/*
-
-$i = 0;
-$c = 0;
-$j = 0;
-foreach ($products as $product):
-    $i++;
-    if ($offset >= $i) {
-        continue;
-    }
-    $c++;
-    $link = CHtml::normalizeUrl(array('/'.$category->slug.'/'.$product->slug.'-'.$product->id));
-    $attachetImage = Attachment::model()->getAttachment('product', $product->id);
-    if ($attachetImage) {
-        $image = CHtml::image(Image::thumb(Yii::app()->params['images'].$attachetImage->image, $this->settings['CATEGORY_PRODUCT_IMAGE_SIZE'], $this->settings['CATEGORY_PRODUCT_IMAGE_SIZE']), $product->content->title);
-    } else {
-        $image = CHtml::image(Image::thumb('/images/fl1.jpg', $this->settings['CATEGORY_PRODUCT_IMAGE_SIZE'], $this->settings['CATEGORY_PRODUCT_IMAGE_SIZE']), $product->content->title);
-    }
 ?>
-        <td class="cell">
-	   <div class="cell-border">
-           <div><?php echo $image; ?></div>
-            <div class="fl-title"><?php echo CHtml::link($product->content->title, $link, array('title' => $product->content->title)); ?></div>
-            <div class="price"><?php echo number_format($product->mainNode->price / $this->currencyValue,2,'.','').Yii::app()->params['currencies'][$this->currency]; ?></div>
-	  </div>
-        </td>
-<?php if ($j == 0): ?>
-        <td style="width:0px;"></td>
-<?php endif;
-$j++;
-if ($c % 2 == 0): ?>
-    </tr>
-    <tr><td colspan="3" style="height:5px;padding:0;"></td></tr>
-    <tr>
-<?php 
-$j= 0;
-endif;
-if ($c == $limit) break;
-endforeach;
-if ($c < 2): ?>
-        <td style="width:0px;">&nbsp;</td>
-        <td></td>
-<?php endif; */ ?>
 </table>
--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -5,38 +5,42 @@
 <?php else: ?>
 <table class="flowers">
 <?php 
+$productCount = count($products);
+$columns = 2;
+$rows = ceil($productCount / $columns);
 $c = 0;
-$j = 0;
-foreach ($products as $product):
-    $c++;
-    $link = CHtml::normalizeUrl(array('/'.$category->slug.'/'.$product->slug.'-'.$product->id));
-    $attachetImage = Attachment::model()->getAttachment('product', $product->id);
-    if ($attachetImage) {
-        $image = CHtml::image(Image::thumb(Yii::app()->params['images'].$attachetImage->image, $this->settings['CATEGORY_PRODUCT_IMAGE_SIZE']), $product->content->title);
-    } else {
-        $image = CHtml::image(Image::thumb('/images/fl1.jpg', $this->settings['CATEGORY_PRODUCT_IMAGE_SIZE']), $product->content->title);
-    }
-?>
+for ($i = 0; $i < $rows; $i++) {
+    echo '<tr>';
+    for ($j = 0; $j < $columns; $j++) {
+        if (!isset($products[$c])) {
+            echo '<td style="width:0px;">&nbsp;</td><td></td>';
+            break;
+        }
+        $product = $products[$c];
+        $link = CHtml::normalizeUrl(array('/'.$category->slug.'/'.$product->slug.'-'.$product->id));
+        $attachetImage = Attachment::model()->getAttachment('product', $product->id);
+        if ($attachetImage) {
+            $image = CHtml::image(Image::thumb(Yii::app()->params['images'].$attachetImage->image, $this->settings['CATEGORY_PRODUCT_IMAGE_WIDTH'], $this->settings['CATEGORY_PRODUCT_IMAGE_HEIGHT']), $product->content->title);
+        } else {
+            $image = CHtml::image(Image::thumb('/images/fl1.jpg', $this->settings['CATEGORY_PRODUCT_IMAGE_WIDTH'], $this->settings['CATEGORY_PRODUCT_IMAGE_HEIGHT']), $product->content->title);
+        }
+        ?>
         <td class="cell">
-<div class="cell-border">
-            <div><?php echo $image; ?></div>
+	   <div class="cell-border">
+           <div><?php echo $image; ?></div>
             <div class="fl-title"><?php echo CHtml::link($product->content->title, $link, array('title' => $product->content->title)); ?></div>
             <div class="price"><?php echo number_format($product->mainNode->price / $this->currencyValue,2,'.','').Yii::app()->params['currencies'][$this->currency]; ?></div>
-</div>
+	  </div>
         </td>
-<!--<?php if ($j == 0): ?>
-        <td style="width:15px;">&nbsp;</td>
-<?php endif; ?> -->
-<?php if ($c % 2 == 0): ?>
-    </tr>
-    <tr><td colspan="3" style="height:15px;"></td></tr>
-    <tr>
-<?php endif;
-$j++;
-endforeach;
-if ($c < 2): ?>
-        <td style="width:15px;">&nbsp;</td>
-        <td></td>
-<?php endif; ?>
+        <?php
+        if ($j == 0) {
+            echo '<td style="width:0px;"></td>';
+        }
+        $c++;
+    }
+    echo '</tr>';
+    echo '<tr><td colspan="3" style="height:5px;padding:0;"></td></tr>';
+}
+?>
 </table>
 <?php endif; ?>
